6e6e0e75989cfbddff8846a39e9489e663c63302
jamiemn86 <jamie.a.mollernielsen@gmail.com>
2021-08-11 18:37:43 +0200

    Iterations 1 and 2 complete and tests passing

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function (a, b) {
+      return a - b;
+    });
+    this.length = this.items.length;
+  }
 
   get(pos) {}
 
